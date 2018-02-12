473a8f387bff4a2dec4e5ecff633e78721830287
Quan Nguyen <manhquan233@gmail.com>
2018-02-12 14:02:58 +0100

    Update mmddCRP.h

--- include/mmddCRP.h
+++ include/mmddCRP.h
@@ -36,15 +36,14 @@ private:
     Eigen::MatrixXd tables_;
     Eigen::VectorXd initmean_;
     Eigen::VectorXd colwiseMin_, colwiseMax_;
+    Eigen::MatrixXd pairwiseDistance_;
     double C_;
     double lambda_;
     double k_;
     double alpha_;
     double gamma_;
     double S_;
-    double maxPossibleDistance_;
     std::vector<std::size_t> data_indices;
-    std::size_t dim_;
 
     boost::random::mt19937 rng_;
 };
